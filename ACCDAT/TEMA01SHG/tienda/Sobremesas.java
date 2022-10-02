aac711e070e8e286d39dd28fea67e6a12382a0cc
Sebastian <32487946+shurtadogar@users.noreply.github.com>
2022-10-02 16:15:46 +0200

    Update Sobremesas.java

--- ACCDAT/TEMA01SHG/tienda/Sobremesas.java
+++ ACCDAT/TEMA01SHG/tienda/Sobremesas.java
@@ -1,28 +1,30 @@
 package entregashg.tienda;
 
 public class Sobremesas extends Ordenadores {
+    /* Atributos */
     private String sTipoTorre;
-
-    public Sobremesas(String sCodigo, int iRam, String sCpu, double dPrecio, String sTipoTorre) {
-        super(sCodigo, iRam, sCpu, dPrecio);
-        this.sTipoTorre = sTipoTorre;
+    /* Constructor recibiendo por parametros los valores */
+    public Sobremesas(String paramsCodigo, int paramiRam, String paramsCpu, double paramdPrecio, String paramsTipoTorre) {
+        super(paramsCodigo, paramiRam, paramsCpu, paramdPrecio);
+        this.sTipoTorre = paramsTipoTorre;
     }
-
+    /* Getters */
     public String getsTipoTorre() {
         return sTipoTorre;
     }
-
-    public void setsTipoTorre(String sTipoTorre) {
-        this.sTipoTorre = sTipoTorre;
+    /* Setters */       
+    public void setsTipoTorre(String paramsTipoTorre) {
+        this.sTipoTorre = paramsTipoTorre;
     }
 
-    
+    /* Método getCaracterísiticas() heredado de Ordenadores.java*/
     @Override
-    public String getCaracterísiticas() {
+    public void getCarateristicas() {
         // TODO Auto-generated method stub
-        return super.getCaracterísiticas()+"\nTipo de torre "+getsTipoTorre();
+        super.getCarateristicas();
+        System.out.println("Tipo de torre: "+getsTipoTorre());
     }
-
+    /* Método toString() para ver los valores  */
     @Override
     public String toString() {
         return "Sobremesas [sTipoTorre=" + sTipoTorre + "]";
