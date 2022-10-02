a7dc7a0dc6b02c3f214eea050d3c0845386faa9f
Sebastian <32487946+shurtadogar@users.noreply.github.com>
2022-10-02 18:08:01 +0200

    Delete ACCDAT/TEMA01SHG directory

--- ACCDAT/TEMA01SHG/puntos/PuntoTest.java
+++
@@ -1,9 +0,0 @@
-package entregashg.puntos;
-
-public class PuntoTest {
-    public static void main(String[] args) {
-        Punto p1 = new Punto();
-        Punto p2 = new Punto(10, 5);
-        System.out.println("Distancia entre puntos "+p1.distancia(p1, p2));
-    }
-}
